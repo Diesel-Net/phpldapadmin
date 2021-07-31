e19b9445e059dd77fae4cc612269138c20ac04d4
Thomas Daley <tomdaley92@gmail.com>
2021-07-31 13:53:07 -0700

    Try adding custom Group template

--- startup.sh
+++ startup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
-TEMPLATE_PATH=/customUser.xml
 
-if [ -f $TEMPLATE_PATH ]; then
-  mv $TEMPLATE_PATH  /var/www/phpldapadmin/templates/creation/
+USER_TEMPLATE_PATH=/customUser.xml
+GROUP_TEMPLATE_PATH=/customGroup.xml
+
+if [ -f $USER_TEMPLATE_PATH ]; then
+  mv $USER_TEMPLATE_PATH  /var/www/phpldapadmin/templates/creation/
+fi
+
+if [ -f $GROUP_TEMPLATE_PATH ]; then
+  mv $GROUP_TEMPLATE_PATH  /var/www/phpldapadmin/templates/creation/
 fi
 
 exit 0
